65a94ab33da8d7a6c025a2eb2d33f78aff70bded
ricfigue21 <ricardo.figueira01@vodafone.com>
2022-07-01 14:46:32 +0100

    add first GET route

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World EXPRESS!')
 })
 
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+    ]
+    app.get('/users', function(req,res){
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
+    })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
